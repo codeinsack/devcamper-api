1d23728d8963bde99e4b494cf1f36faeb89d2df9
codeinsack <codeinsack@gmail.com>
2020-10-08 00:46:06 +0300

    add protected middleware

--- routes/courses.js
+++ routes/courses.js
@@ -12,6 +12,8 @@ const advancedResults = require('../middleware/advancedResults');
 
 const router = express.Router({ mergeParams: true });
 
+const { protect } = require('../middleware/auth');
+
 router.route('/')
   .get(advancedResults(Course, {
     path: 'bootcamp',
@@ -19,10 +21,10 @@ router.route('/')
   }),
     getCourses
   )
-  .post(addCourse);
+  .post(protect, addCourse);
 router.route('/:id')
   .get(getCourse)
-  .put(updateCourse)
-  .delete(deleteCourse);
+  .put(protect, updateCourse)
+  .delete(protect, deleteCourse);
 
 module.exports = router;
